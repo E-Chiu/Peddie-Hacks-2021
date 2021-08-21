4ee54b8c14e122f6c01638576b14797ad099bd98
tsabkuro <t.sabanshi@gmail.com>
2021-08-21 15:17:36 -0700

    Added profile input

--- gui.py
+++ gui.py
@@ -9,7 +9,8 @@ sg.SetOptions(background_color='#000',
     button_color=('black','#FFF'))
 
 encryptColumn = [
-    [sg.Combo([], size=(30,1), key="_PROFILES_"), sg.Button("Add New Profile", key="_NEW-PROF_", size=(15,1))],
+    [sg.In(size=(32,1), key="_NEW-PROF-IN_", enable_events=True), sg.Button("Add New Profile", key="_NEW-PROF_", size=(15,1))],
+    [sg.Combo([], size=(30,1), key="_PROFILES_"), sg.Button("Remove Current Profile", key="_REMOVE-PROF_", size=(20,1))],
     [sg.In(size=(70,1), key="_FILES_"), sg.FilesBrowse("Add Files", target="_FILES_")], #values returns the path to files
     [sg.Submit("Add")],
     [sg.Listbox(values=[], enable_events=True, size=(70, 10), key="_FILE-ENCRYPT-LIST_")],
@@ -24,7 +25,6 @@ layout = [
 
 window = sg.Window(title="File Encryptor", layout=layout, margins=(10, 50), location=(0,0), size=(1150, 600)) #window properties
 
-
 filesAdded = []
 while True:
     event, values = window.read()
@@ -32,8 +32,14 @@ while True:
     if event == sg.WIN_CLOSED:
         break
 
+    if len(values["_NEW-PROF-IN_"]) >= 30:
+        window["_NEW-PROF-IN_"].update(values["_NEW-PROF-IN_"][:-1])
+
+    if event == "_NEW-PROF-IN_":
+        continue
+
     if event == "_NEW-PROF_":
-        window["_PROFILES_"].update(values=)
+        continue
 
     if event == "Add":
         filesAdded += values['_FILES_'].split(';') #adding new file paths
