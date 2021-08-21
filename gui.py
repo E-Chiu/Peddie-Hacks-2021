adbec1c74e410a2e459323b9f81a6ec8047e8afd
tsabkuro <t.sabanshi@gmail.com>
2021-08-21 16:17:59 -0700

    Able to add and remove files

--- gui.py
+++ gui.py
@@ -1,5 +1,8 @@
 import PySimpleGUI as sg
-import main_Backend as crypt
+import backend as crypt
+from cryptography.fernet import Fernet
+from os import listdir
+from os.path import isfile, join
 
 sg.SetOptions(background_color='#000',
     text_element_background_color='#9FB8AD',
@@ -8,13 +11,19 @@ sg.SetOptions(background_color='#000',
     input_elements_background_color='#FFF',
     button_color=('black','#FFF'))
 
+
+# mypath = "./profiles/"
+#         onlyfiles = [i for i in listdir(mypath) if isfile(join(mypath, i))]
+#         print(onlyfiles)
+
+
 encryptColumn = [
-    [sg.In(size=(32,1), key="_NEW-PROF-IN_", enable_events=True), sg.Button("Add New Profile", key="_NEW-PROF_", size=(15,1))],
-    [sg.Combo([], size=(30,1), key="_PROFILES_"), sg.Button("Remove Current Profile", key="_REMOVE-PROF_", size=(20,1))],
-    [sg.In(size=(70,1), key="_FILES_"), sg.FilesBrowse("Add Files", target="_FILES_")], #values returns the path to files
-    [sg.Submit("Add")],
-    [sg.Listbox(values=[], enable_events=True, size=(70, 10), key="_FILE-ENCRYPT-LIST_")],
-    [sg.Submit("Encrypt"), sg.Submit("Decrypt")]
+    [sg.In(size=(32,1), key="_NEW-PROF-IN_", enable_events=True), sg.Button("Add New Profile", key="_NEW-PROF_", size=(15,1))], # create new profile
+    [sg.Combo([i for i in listdir("./profiles/") if isfile(join("./profiles/", i))], size=(30,1), key="_PROFILES_", enable_events=True), sg.Button("Remove Current Profile", key="_REMOVE-PROF_", size=(20,1))], # choose profile
+    [sg.In(size=(70,1), key="_FILES_"), sg.FilesBrowse("Add Files", target="_FILES_")], # browse files to encrypt
+    [sg.Submit("Add")], # adds chosen files to profile
+    [sg.Listbox(values=[], enable_events=True, size=(70, 10), key="_FILE-ENCRYPT-LIST_")], # files in the profile
+    [sg.Submit("Encrypt"), sg.Submit("Decrypt")] # encrypt or decrypt files in profile
 ]
 
 #window format
@@ -26,20 +35,32 @@ layout = [
 window = sg.Window(title="File Encryptor", layout=layout, margins=(10, 50), location=(0,0), size=(1150, 600)) #window properties
 
 filesAdded = []
+
+# obtain key from file
+with open('filekey.key', 'r') as filekey:
+    key = filekey.read()
+with open('identifier.key', 'rb') as identifierkey:
+    identifier = identifierkey.read()
+# use key
+fernet = Fernet(key)
+
 while True:
     event, values = window.read()
 
     if event == sg.WIN_CLOSED:
         break
 
-    if len(values["_NEW-PROF-IN_"]) >= 30:
+    if len(values["_NEW-PROF-IN_"]) >= 30: # maximum character limit is 30 for files
         window["_NEW-PROF-IN_"].update(values["_NEW-PROF-IN_"][:-1])
 
-    if event == "_NEW-PROF-IN_":
-        continue
-
     if event == "_NEW-PROF_":
-        continue
+        crypt.add_Profile(fernet, identifier, values["_NEW-PROF-IN_"]) # creates a profile
+        window["_PROFILES_"].update(values=[i for i in listdir("./profiles/") if isfile(join("./profiles/", i))])
+
+    if event == "_REMOVE-PROF_":
+        crypt.del_Profile(fernet, identifier, values["_PROFILES_"])
+        window["_PROFILES_"].update(values=[i for i in listdir("./profiles/") if isfile(join("./profiles/", i))])
+
 
     if event == "Add":
         filesAdded += values['_FILES_'].split(';') #adding new file paths
